93e29353f73106e170babbb0f5329dd6707baf92
Marcos André de Souza Filho <73143411+AvocadoDevOps@users.noreply.github.com>
2021-03-22 00:02:15 -0700

    up completo do projeto v1.0.0

---
+++ YourIMC/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YourIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 Form2 = new Form2();
+            Form2.Show();
+        }
+    }
+}
